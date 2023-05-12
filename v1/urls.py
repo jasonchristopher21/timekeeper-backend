20db13caa25dcdee6874cbadfbd31f2f5a83d404
Jason Christopher <jason.christopher@u.nus.edu>
2023-05-12 20:53:53 +0800

    Implement REST API to Deck and Slide

--- v1/urls.py
+++ v1/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-
+from django.urls import include, path
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'decks', views.DeckViewSet)
+router.register(r'slides', views.SlideViewSet)
+
+# Wire up our API using autiomatic URL routing.
+# Additionally, we include login URLs for the browsable API
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- v1/views.py
+++ v1/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from .serializers import DeckSerializer, SlideSerializer
+from .models import Deck, Slide
+
 # Create your views here.
+class DeckViewSet(viewsets.ModelViewSet):
+    queryset = Deck.objects.all().order_by('name')
+    serializer_class = DeckSerializer
+
+class SlideViewSet(viewsets.ModelViewSet):
+    queryset = Slide.objects.all().order_by('slide_number')
+    serializer_class = SlideSerializer
